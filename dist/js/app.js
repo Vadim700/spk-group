42d3d5f8fb8afd940fe1451ce733f06375ac53dd
Vadim <bekmansurov700@mail.ru>
2023-07-02 00:32:16 +0500

    added js styles

--- dist/js/app.js
+++ dist/js/app.js
@@ -273,18 +273,45 @@
     menuHeader.addEventListener("click", (event => {
         const target = event.target;
         menuHeader?.querySelector(".menu__grid");
+        const fullOpen = [ {
+            clip: "rect(0, 263px, 260px, 0)"
+        }, {
+            clip: "rect(0, 263px, 520px, 0)"
+        }, {
+            clip: "rect(0, 655px, 520px, 0)"
+        } ];
+        const hulfOpen = [ {
+            clip: "rect(0, 263px, 0, 0)"
+        }, {
+            clip: "rect(0, 263px, 260px, 0)"
+        }, {
+            clip: "rect(0, 263px, 260px, 0)"
+        } ];
+        const fullClose = [ {
+            clip: "rect(0, 655px, 520px, 0)"
+        }, {
+            clip: "rect(0, 263px, 520px, 0)"
+        }, {
+            clip: "rect(0, 263px, 0px, 0)"
+        } ];
+        const options = {
+            duration: 700,
+            iterations: 1,
+            fill: "forwards",
+            easing: "ease-out"
+        };
         if (target.closest(".menu__item")) {
             menuHeader.querySelectorAll(".menu__item").forEach((item => item.classList.remove("active")));
             target.parentNode.classList.toggle("active");
             document.querySelectorAll(".menu__grid").forEach((item => item.style.animation = ""));
-            target.parentElement.lastElementChild.style.animation = "menuHulfOpen 0.7s ease-out forwards 0s";
+            target.parentElement.lastElementChild.animate(hulfOpen, options);
         }
         if (target.closest(".menu__subitem")) {
             document.querySelectorAll(".menu__subitem").forEach((item => item.classList.remove("selected")));
             target.parentNode.classList.add("selected");
-            target.parentElement.parentElement.parentElement.style.animation = "menuFullOpen 0.7s ease-out forwards 0s";
+            target.parentElement.parentElement.parentElement.animate(fullOpen, options);
         }
-        if (target.closest(".menu__sub-subitem")) target.parentElement.parentElement.parentElement.style.animation = "menuFullClose 0.7s ease-out forwards 0s";
+        if (target.closest(".menu__sub-subitem")) target.parentElement.parentElement.parentElement.animate(fullClose, options);
     }));
     window["FLS"] = true;
     isWebp();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -31,13 +31,41 @@ menuHeader.addEventListener("click", event => {
 	const target = event.target;
 	const menuGrid = menuHeader?.querySelector('.menu__grid');
 
+	const fullOpen = [
+		{ clip: 'rect(0, 263px, 260px, 0)' },
+		{ clip: 'rect(0, 263px, 520px, 0)' },
+		{ clip: 'rect(0, 655px, 520px, 0)' }
+	];
+
+	const hulfOpen = [
+		{ clip: 'rect(0, 263px, 0, 0)' },
+		{ clip: 'rect(0, 263px, 260px, 0)' },
+		{ clip: 'rect(0, 263px, 260px, 0)' },
+	];
+
+	const fullClose = [
+		{ clip: 'rect(0, 655px, 520px, 0)' },
+		{ clip: 'rect(0, 263px, 520px, 0)' },
+		{ clip: 'rect(0, 263px, 0px, 0)' },
+	];
+
+	const halfClose = [];
+
+	const options = {
+		duration: 700,
+		iterations: 1,
+		fill: "forwards",
+		easing: "ease-out",
+	};
+
 	if (target.closest('.menu__item')) {
-		menuHeader.querySelectorAll('.menu__item').forEach(item => item.classList.remove('active'));
+		menuHeader.querySelectorAll('.menu__item')
+			.forEach(item => item.classList.remove('active'));
 		target.parentNode.classList.toggle('active');
 
 		document.querySelectorAll('.menu__grid')
 			.forEach(item => item.style.animation = '')
-		target.parentElement.lastElementChild.style.animation = 'menuHulfOpen 0.7s ease-out forwards 0s';
+		target.parentElement.lastElementChild.animate(hulfOpen, options)
 	}
 
 	if (target.closest('.menu__subitem')) {
@@ -45,13 +73,18 @@ menuHeader.addEventListener("click", event => {
 			.forEach(item => item.classList.remove('selected'));
 		target.parentNode.classList.add('selected');
 
-		target.parentElement.parentElement.parentElement
-			.style.animation = 'menuFullOpen 0.7s ease-out forwards 0s';
+		target.parentElement.parentElement.parentElement.animate(fullOpen, options)
 	}
 
 	if (target.closest('.menu__sub-subitem')) {
-		target.parentElement.parentElement.parentElement
-			.style.animation = 'menuFullClose 0.7s ease-out forwards 0s';
+		target.parentElement.parentElement.parentElement.animate(fullClose, options)
+
 	}
 });
+
+
+
+
+
+
 
