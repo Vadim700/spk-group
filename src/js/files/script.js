82550c532fc4dd8ef3ac744adb3a3ea5a0d1fcad
Vadim <bekmansurov700@mail.ru>
2023-11-08 00:29:39 +0500

    starting footer

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -9,18 +9,70 @@ document.querySelector('.actions-header').addEventListener('click', event => {
 	const target = event.target;
 	const locationList = document.querySelector('.actions-header__list');
 
-	if (target.closest('.actions-header__item_icon_location')) {
-		locationList.classList.toggle('open');
+	let count = 0;
+	if (target.closest('.actions-header__item_icon_location') || target.closest('.actions-header__item_location')) {
+		for (let i of locationList.children) {
+			count += i.clientHeight + parseInt(window.getComputedStyle(i).marginBottom);
+		}
+
+		count + 30;
+
+		locationList.animate([
+			{ clip: 'rect(0, 150px, 0px, 0)' },
+			{ clip: `rect(0, 150px, ${count / 2}px, 0)` },
+			{ clip: `rect(0, 150px, ${count}px, 0)` },
+		], {
+			duration: 250,
+			iterations: 1,
+			fill: "forwards",
+		})
 	} else {
-		locationList.classList.remove('open');
+		for (let i of locationList.children) {
+			count += i.clientHeight + parseInt(window.getComputedStyle(i).marginBottom);
+		}
+		locationList.animate([
+			{ clip: `rect(0, 150px, ${count}px, 0)` },
+			{ clip: `rect(0, 150px, ${count / 2}px, 0)` },
+			{ clip: 'rect(0, 150px, 0px, 0)' },
+		], {
+			duration: 250,
+			iterations: 1,
+			fill: "forwards",
+		})
 	}
+});
+
+
+// onClick location
+const actionHeader = document.querySelector('.actions-header');
+
+const toUp = () => {
+	actionHeader.querySelector('.actions-header__item_icon_location')
+		.style.transform = 'translateY(-5px)';
+}
+
+actionHeader
+	.querySelector('.actions-header__item_location')
+	.addEventListener('mouseover', toUp);
+
+actionHeader.querySelector('.actions-header__item_location').addEventListener('mouseout', () => {
+	actionHeader.querySelector('.actions-header__item_icon_location')
+		.style.transform = '';
 });
 
 
 // onClickEscape
 document.addEventListener('keydown', event => {
 	if (event.code === 'Escape') {
-		document.querySelector('.actions-header__list').classList.remove('open');
+		document.querySelector('.actions-header__list').animate([
+			{ clip: 'rect(0, 150px, 500px, 0)' },
+			{ clip: 'rect(0, 150px, 250px, 0)' },
+			{ clip: 'rect(0, 150px, 0px, 0)' },
+		], {
+			duration: 0,
+			iterations: 1,
+			fill: "forwards",
+		});
 
 		document.querySelectorAll('.menu__grid').forEach(item => item.animate([
 			{ clip: 'rect(0, 655px, 520px, 0)' },
